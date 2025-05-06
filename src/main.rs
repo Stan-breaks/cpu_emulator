fddf04f93167ba2829701d81951095b02591e7f9
Stan-breaks <stanleymwendwa03@gmail.com>
2025-05-06 12:15:04 +0300

    most significant bit and endinness

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-#[allow(arithmetic_overflow)]
+use std::mem::transmute;
 fn main() {
-    let (a, b) = (200, 200);
-    let c: u8 = a + b;
-    println!("200 + 200 = {}", c);
+    let big_endian: [u8; 4] = [0xAA, 0xBB, 0xCC, 0xDD];
+    let little_endian: [u8; 4] = [0xDD, 0xCC, 0xBB, 0xAA];
+    let a: i32 = unsafe { transmute(big_endian) };
+    let b: i32 = unsafe { transmute(little_endian) };
+    println!("{} vs {}", a, b);
 }
